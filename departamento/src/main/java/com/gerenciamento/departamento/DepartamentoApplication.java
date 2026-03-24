b7dc79adc2ef5bbc586958f943c34fa0c0228dad
salomoa <salomaoantoniobr@gmail.com>
2026-03-24 18:29:32 -0300

    inicializador

---
+++ departamento/src/main/java/com/gerenciamento/departamento/DepartamentoApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciamento.departamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartamentoApplication.class, args);
+	}
+
+}
---
+++ departamento/src/test/java/com/gerenciamento/departamento/DepartamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciamento.departamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
