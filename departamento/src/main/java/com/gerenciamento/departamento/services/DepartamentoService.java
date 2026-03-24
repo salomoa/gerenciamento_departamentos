93744ea845383d8eb8966ed5ed9887473bff224a
salomoa <salomaoantoniobr@gmail.com>
2026-03-24 18:37:59 -0300

    service

---
+++ departamento/src/main/java/com/gerenciamento/departamento/services/DepartamentoService.java
@@ -0,0 +1,35 @@
+package com.gerenciamento.departamento.services;
+
+import com.gerenciamento.departamento.models.DepartamentoModel;
+import com.gerenciamento.departamento.repositories.DepartamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class DepartamentoService {
+
+    @Autowired
+    private DepartamentoRepository departamentoRepository;
+
+    public List<DepartamentoModel> findAll() { return departamentoRepository.findAll(); }
+
+    public DepartamentoModel criarPessoa(DepartamentoModel departamentoModel){
+        return departamentoRepository.save(departamentoModel);
+    }
+
+    public Optional<DepartamentoModel> buscarId(Long id) { return departamentoRepository.findById(id); }
+
+    public DepartamentoModel atualizar(Long id, DepartamentoModel departamentoModel){
+        DepartamentoModel model = departamentoRepository.findById(id).get();
+        model.setNome(departamentoModel.getNome());
+        model.setLocalizacao(departamentoModel.getLocalizacao());
+        return departamentoRepository.save(model);
+    }
+
+    public void deletar(Long id){ departamentoRepository.deleteById(id); }
+
+}
